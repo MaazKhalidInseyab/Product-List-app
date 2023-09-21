869975b889f740f31fb22bdcd0288559deb1aaa8
MaazKhalidInseyab <maaz.khalid@inseyab.com>
2023-09-21 16:02:04 +0500

    Layout builder used

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -17,18 +17,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  static const platform = const
-  MethodChannel('flutterapp.tutorialspoint.com/browser');
+class portraitView extends StatelessWidget {
+  const portraitView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title),
-      ),
-      body: ListView(
+    return Center(
+      child: ListView(
         shrinkWrap: true,
         padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
         children: [
@@ -93,6 +88,39 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+class landscapeView extends StatelessWidget {
+  const landscapeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue, child: portraitView());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  // static const platform = const
+  // MethodChannel('flutterapp.tutorialspoint.com/browser');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.title),
+      ),
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 500) {
+          return landscapeView();
+        } else {
+          return portraitView();
+        }
+      }),
+    );
+  }
+}
+
 class ProductBox extends StatelessWidget {
   ProductBox(
       {Key? key,
@@ -105,32 +133,34 @@ class ProductBox extends StatelessWidget {
   final String description;
   final int price;
   final String image;
-  show(BuildContext context){
+
+  show(BuildContext context) {
     showDialog(
-      context: context, builder: (BuildContext context) {
-      // return object of type Dialog
-      return AlertDialog(
-        title: new Text("Price"),
-        content: new Text("\$$price"),
-        actions: <Widget>[
-           ElevatedButton(
-            child: new Text("Close"),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Price"),
+          content: new Text("\$$price"),
+          actions: <Widget>[
+            ElevatedButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
-
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){show(context);},
+      onTap: () {
+        show(context);
+      },
       child: Container(
-
           padding: EdgeInsets.all(2),
           height: 120,
           child: Card(
@@ -140,20 +170,22 @@ class ProductBox extends StatelessWidget {
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                  Image.asset("assets/images/" + image),
-                  Expanded(
-                      child: Container(
-                          padding: EdgeInsets.all(5),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text(this.name,
-                                  style: TextStyle(fontWeight: FontWeight.bold)),
-                              Text(this.description),
-                              Text("tap for price"),
-                            ],
-                          )))
-                ]),
+                      Image.asset("assets/images/" + image),
+                      Expanded(
+                          child: Container(
+                              padding: EdgeInsets.all(5),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Text(this.name,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  Text(this.description),
+                                  Text("tap for price"),
+                                ],
+                              )))
+                    ]),
               ))),
     );
   }
